a39ec332ceb0b17a40ce183bc27bf3bd060316c3
Brooke <brooke.leichtle@gmail.com>
2022-10-25 11:23:48 -0400

    fix for node/react projects

--- nvim/lua/my/telescope.lua
+++ nvim/lua/my/telescope.lua
@@ -7,10 +7,12 @@ local actions = require "telescope.actions"
 
 telescope.setup {
   defaults = {
-
     prompt_prefix = " ",
     selection_caret = " ",
     path_display = { "smart" },
+    file_ignore_patterns = {
+      "node_modules"
+    },
     mappings = {
       i = {
         ["<C-n>"] = actions.cycle_history_next,
