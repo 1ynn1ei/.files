73ac3fa1fde92f902269f89c5dee2dc1f9b2e131
root <root@shiro>
2023-04-22 14:12:11 +0200

    fixing typo

--- install.sh
+++ install.sh
@@ -16,15 +16,15 @@ install_if_not_exist fuse
 
 # default our shell
 chsh -s $(which zsh)
-zsh
 
 # install neovim
 wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
 chmod u+x nvim.appimage
-mv nvim.appimage/bin/nvim
+mv nvim.appimage /usr/bin/nvim
 
 # link up our configs
 stow -v -R -t ~/.config config
 stow -v -R -t ~ zsh
 
+zsh
 
