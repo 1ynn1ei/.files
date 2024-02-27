dff015724dfb34d2ba7d01928577e2f8a7842a78
Lynn Leichtle <brooke.leichtle@gmail.com>
2024-02-27 20:02:59 +0100

    install script and adding a bunch of git related goodies

--- config/after/plugin/vimtex.lua
+++
@@ -1,6 +0,0 @@
-vim.g.vimtex_view_general_viewer = 'zathura'
-vim.g.vimtex_view_method = 'zathura'
-vim.g.vimtex_view_automatic=1
-vim.g.vimtex_view_enabled = 1
-vim.g.tex_flavor='latex'
-vim.g.vimtex_latexmk_continuous = 1
